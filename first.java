7759f0ccfacf4336187dbcf27d17c7c016ad1f0e
vasanthsourav <vasanthsourav@gmail.com>
2025-11-11 22:15:16 +0530

    Add files via upload
    added on NOV 11,2025

---
+++ first.java
@@ -0,0 +1,11 @@
+public class first {
+    public static void main(String[] args) {
+        System.out.println("This is MY first Java file.");
+        boolean ISFUN = false;
+        if (ISFUN==true) {
+            System.out.println("Java is fun!");
+        } else {
+            System.out.println("Java is not fun!");
+        }
+    }
+}
---
+++ test.java
@@ -0,0 +1,12 @@
+public class test {
+    public static void main(String[] args) {
+    int test=10;
+if (test==10) {
+    System.out.println("This is error");
+}
+if (test==10) {
+    System.out.println("This is another error");
+}
+}
+}
+
